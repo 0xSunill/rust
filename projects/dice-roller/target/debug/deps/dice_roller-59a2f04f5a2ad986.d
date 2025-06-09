974a5f3952e83edc4fd2a25d75e58570d136f572
0xSunill <sunday7637@gmail.com>
2025-06-09 17:53:26 +0000

    dice roller

---
+++ projects/dice-roller/target/debug/deps/dice_roller-59a2f04f5a2ad986.d
@@ -0,0 +1,5 @@
+/root/blockchain/solana/rust/projects/dice-roller/target/debug/deps/libdice_roller-59a2f04f5a2ad986.rmeta: src/main.rs
+
+/root/blockchain/solana/rust/projects/dice-roller/target/debug/deps/dice_roller-59a2f04f5a2ad986.d: src/main.rs
+
+src/main.rs:
---
+++ projects/dice-roller/target/debug/deps/dice_roller-adedce3946b04dcd.d
@@ -0,0 +1,5 @@
+/root/blockchain/solana/rust/projects/dice-roller/target/debug/deps/libdice_roller-adedce3946b04dcd.rmeta: src/main.rs
+
+/root/blockchain/solana/rust/projects/dice-roller/target/debug/deps/dice_roller-adedce3946b04dcd.d: src/main.rs
+
+src/main.rs:
