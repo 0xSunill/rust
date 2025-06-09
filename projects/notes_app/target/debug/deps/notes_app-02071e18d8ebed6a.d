2a140e2739b2e2ccad9c3a4219c7bf97ed0b3166
0xSunill <sunday7637@gmail.com>
2025-06-09 17:49:53 +0000

    notes app

---
+++ projects/notes_app/target/debug/deps/notes_app-02071e18d8ebed6a.d
@@ -0,0 +1,5 @@
+/root/blockchain/solana/rust/projects/notes_app/target/debug/deps/libnotes_app-02071e18d8ebed6a.rmeta: src/main.rs
+
+/root/blockchain/solana/rust/projects/notes_app/target/debug/deps/notes_app-02071e18d8ebed6a.d: src/main.rs
+
+src/main.rs:
---
+++ projects/notes_app/target/debug/deps/notes_app-e1efc4afdd265e88.d
@@ -0,0 +1,5 @@
+/root/blockchain/solana/rust/projects/notes_app/target/debug/deps/libnotes_app-e1efc4afdd265e88.rmeta: src/main.rs
+
+/root/blockchain/solana/rust/projects/notes_app/target/debug/deps/notes_app-e1efc4afdd265e88.d: src/main.rs
+
+src/main.rs:
