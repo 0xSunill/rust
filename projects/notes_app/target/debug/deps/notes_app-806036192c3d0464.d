4eae0b88043ac44317eb44b0cc2c63b4d11caf85
0xSunill <sunday7637@gmail.com>
2025-06-09 17:52:19 +0000

    final notes

---
+++ projects/notes_app/target/debug/deps/notes_app-806036192c3d0464.d
@@ -0,0 +1,5 @@
+/root/blockchain/solana/rust/projects/notes_app/target/debug/deps/notes_app-806036192c3d0464: src/main.rs
+
+/root/blockchain/solana/rust/projects/notes_app/target/debug/deps/notes_app-806036192c3d0464.d: src/main.rs
+
+src/main.rs:
---
+++ projects/notes_app/target/debug/notes_app.d
@@ -0,0 +1,1 @@
+/root/blockchain/solana/rust/projects/notes_app/target/debug/notes_app: /root/blockchain/solana/rust/projects/notes_app/src/main.rs
