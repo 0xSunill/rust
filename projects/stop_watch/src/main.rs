a49567d085f7bbf2f4ad4de212de707c5bb3ed3f
0xSunill <sunday7637@gmail.com>
2025-06-12 16:41:15 +0000

    stop watch final

--- projects/stop_watch/src/main.rs
+++ projects/stop_watch/src/main.rs
@@ -1,3 +1,15 @@
+use std::time::Instant;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Press ENTER to start the timer...");
+    let _ = io::stdin().read_line(&mut String::new());
+
+    let start = Instant::now();
+
+    println!("Press ENTER to stop the timer...");
+    let _ = io::stdin().read_line(&mut String::new());
+
+    let duration = start.elapsed();
+    println!("Time elapsed: {:.2?}", duration);
 }
